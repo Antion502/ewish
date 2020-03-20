5e2bc3bcd58902f436d0fe682a819b9c4dfc3c2b
wuyuxun <yuxun.wu@aqara.com>
2020-03-20 17:25:00 +0800

    Merchandise commit

---
+++ ewish-merchandise/src/main/java/pers/antion/ewish/merchandise/domain/repository/MerchandiseRepository.java
@@ -0,0 +1,9 @@
+package pers.antion.ewish.merchandise.domain.repository;
+
+/**
+ * @创建人 yuxun.wu
+ * @创建时间 2020/3/20
+ * @描述
+ **/
+public interface MerchandiseRepository {
+}
---
+++ ewish-merchandise/src/main/java/pers/antion/ewish/merchandise/domain/service/MerchandiseDomainService.java
@@ -0,0 +1,9 @@
+package pers.antion.ewish.merchandise.domain.service;
+
+/**
+ * @创建人 yuxun.wu
+ * @创建时间 2020/3/20
+ * @描述
+ **/
+public interface MerchandiseDomainService {
+}
